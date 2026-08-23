09360fbf71c04b3423409ff565822998a0079474
Matthew Reardon <patriotnewsactivism@proton.me>
2026-08-23 19:26:15 -0400

    Centralize verified payment destinations

---
+++ src/config/paymentLinks.ts
@@ -0,0 +1,10 @@
+export const CASHAPP_HANDLE = "$1Aaudit";
+export const CASHAPP_URL = "https://cash.app/$1Aaudit";
+
+export const VENMO_HANDLE = "@WeThePeopleNews";
+export const VENMO_URL = "https://venmo.com/WeThePeopleNews";
+
+export const STRIPE_DONATION_URL = import.meta.env.VITE_STRIPE_DONATION_URL as string | undefined;
+
+export const PRIMARY_DONATION_URL = STRIPE_DONATION_URL ?? CASHAPP_URL;
+export const PRIMARY_DONATION_LABEL = STRIPE_DONATION_URL ? "Stripe" : "CashApp";

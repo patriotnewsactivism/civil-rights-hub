bed56409c1dfd757e7601a098eb061712d8530dd
Matthew Reardon <patriotnewsactivism@proton.me>
2026-08-28 17:37:41 -0500

    Initialize Sentry before rendering app

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import ErrorBoundary from "./components/ErrorBoundary";
 import EnvironmentCheck from "./components/EnvironmentCheck";
+import { initSentry } from "./lib/sentry";
 import "./index.css";
+
+initSentry();
 
 createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
